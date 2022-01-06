f9ab368ccd1105395dcd76ae867d14ccd82c88d3
unknown <christian.llumiquinga@epn.edu.ec>
2022-01-05 19:32:17 -0500

    servicesos camara

---
+++ src/app/services/photo-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+import { Platform } from '@ionic/angular';
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoServiceService {
+
+  constructor() { }
+}
